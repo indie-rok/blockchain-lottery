93eb1cc16791925a31b20c74bb872fd87764a04e
Emmanuel Orozco <emmanuel@laboratoria.la>
2018-04-09 16:19:11 -0500

    added needed dependencies and fixed tdd v1

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -1,6 +1,12 @@
 const assert = require("assert");
 const ganache = require("ganache-cli");
-
 const { interface, bytecode } = require("../build");
+const Web3 = require("web3");
 
-console.log(interface);
+let web3 = new Web3(ganache.provider());
+
+describe("Contract creation", () => {
+  it("succesfully creates the contract", async () => {
+    let accounts = await web3.eth.getAccounts();
+  });
+});
